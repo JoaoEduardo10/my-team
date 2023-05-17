f35ef17d5c654ee26b93c93b3ea080c1eb65a9ed
João Eduardo <joaoeduardoj920@gmail.com>
2023-05-17 20:18:31 -0300

    feat: adding image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-export const App = () => {
+export const Home = () => {
   return <div>Hello Word</div>;
 };
 
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom';
-import {App} from '../pages/App';
+import { Home } from '../pages/Home';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />
+    element: <Home />
   }
 ]);
-
-
