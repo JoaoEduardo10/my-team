7b6462e41353f43103b7b8253b91057a72c7c3c0
João Eduardo <joaoeduardoj920@gmail.com>
2023-05-17 21:39:55 -0300

    feat: ajust styles globals end styles theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,10 @@
 const theme = {
   colors: {
-    red: "red",
+    white: '#000',
+    black: '#222',
+    gray: '#9b9b9b',
+    lightBlue: '#11998e',
+    lightGreen: '#38ef7d'
   },
 } as const;
 
