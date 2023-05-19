c57281825e02ab70fa3c32ff3e0f905644d17388
João Eduardo <joaoeduardoj920@gmail.com>
2023-05-19 14:33:04 -0300

    refacory(Button): ajust media querys

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -5,19 +5,44 @@ export const Button = styled.button`
     border: none;
     width: 17rem;
     padding: ${theme.padding.small};
-    background-image: linear-gradient(to right, ${theme.colors.lightBlue}, ${theme.colors.lightGreen});
     border-radius: 0.7rem;
-    color: ${theme.colors.white};
     font-size: ${theme.padding.small};
     font-weight: bold;
     cursor: pointer;
     transition: all 300ms ease-in-out;
+    background-image: linear-gradient(to right, ${theme.colors.lightBlue}, ${theme.colors.lightGreen});
+    color: ${theme.colors.white};
 
     &:hover {
       background-image: none;
       background-color: ${theme.colors.white};
       color: ${theme.colors.black};
       border: .1rem solid ${theme.colors.lightBlue};
+    }
+
+    ${theme.media_screen_size.phone} {
+      &:hover {
+        background-image: linear-gradient(to right, ${theme.colors.lightBlue}, ${theme.colors.lightGreen});
+        color: ${theme.colors.white};
+        border: none;
+      }
+    }
+
+    ${theme.media_screen_size.tablet} {
+      &:hover {
+        background-image: linear-gradient(to right, ${theme.colors.lightBlue}, ${theme.colors.lightGreen});
+        color: ${theme.colors.white};
+        border: none;
+      }
+    }
+
+    ${theme.media_screen_size.lapTop} {
+      &:hover {
+      background-image: none;
+      background-color: ${theme.colors.white};
+      color: ${theme.colors.black};
+      border: .1rem solid ${theme.colors.lightBlue};
+    }
     }
   `}
 `;
