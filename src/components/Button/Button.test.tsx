84f35f4788c91eeba12dc9b871c70a631df671d4
João Eduardo <joaoeduardoj920@gmail.com>
2023-05-18 21:56:16 -0300

    feat(Button): adding test

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,44 @@
+import { fireEvent, screen } from '@testing-library/react';
+import { Button } from '.';
+import { renderTheme } from '../../utils/renderTheme';
+import { vi } from 'vitest';
+
+describe('<Button />', () => {
+  it('should a render button', () => {
+    renderTheme(<Button text='test' />);
+
+    const button = screen.getByRole('button', { name: 'test' });
+
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should click the button', () => {
+    const onClick = vi.fn();
+
+    renderTheme(<Button text='test' onClick={onClick} />);
+
+    const button = screen.getByRole('button', { name: 'test' });
+
+    expect(button).toBeInTheDocument();
+
+    fireEvent.click(button);
+
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  it('should match snapshot', () => {
+    const onClick = vi.fn();
+
+    renderTheme(<Button text='test' onClick={onClick} />);
+
+    const button = screen.getByRole('button', { name: 'test' });
+
+    expect(button).toMatchInlineSnapshot(`
+      <button
+        class="dTJCgP"
+      >
+        test
+      </button>
+    `);
+  });
+});
