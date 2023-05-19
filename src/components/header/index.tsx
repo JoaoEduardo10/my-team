131003ad49a9b48231ec72d4abeffc449739c4d9
João Eduardo <joaoeduardoj920@gmail.com>
2023-05-19 14:31:10 -0300

    feat(Header): create component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,27 @@
+import * as Styled from './styles';
+import logo from '../../../public/images/logo.png';
+import { Button } from '../Button';
+import { useContext } from 'react';
+import { AuthContext } from '../../context/auth';
+
+export const Header = () => {
+  const { signOut } = useContext(AuthContext);
+
+  const handleSignOut = () => {
+    signOut();
+  };
+
+  return (
+    <Styled.Header>
+      <Styled.Conteiner>
+        <Styled.ConteinerLogo>
+          <img src={logo} alt="logo" />
+        </Styled.ConteinerLogo>
+        <Styled.Heading>Meu Time</Styled.Heading>
+      </Styled.Conteiner>
+      <Styled.Conteiner>
+        <Button onClick={handleSignOut} text='Sair' />
+      </Styled.Conteiner>
+    </Styled.Header>
+  );
+};
