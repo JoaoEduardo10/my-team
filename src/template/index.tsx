99172c5c0f74634aa6554f7f73be773df00d476e
João Eduardo <joaoeduardoj920@gmail.com>
2023-05-19 14:31:36 -0300

    feat(template): create component

---
+++ src/template/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from '../components/header';
+import * as Styled from './styles';
+
+export const Template = () => {
+  return (
+    <Styled.Conteiner>
+      <Header />
+    </Styled.Conteiner>
+  );
+};
