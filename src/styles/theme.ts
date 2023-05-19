60094218b1c29442b2967187e0b98f94f2f07156
João Eduardo <joaoeduardoj920@gmail.com>
2023-05-18 22:02:48 -0300

    refactory(theme): ajust color white

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,10 +1,29 @@
 const theme = {
   colors: {
-    white: '#000',
+    white: '#fff',
     black: '#222',
     gray: '#9b9b9b',
     lightBlue: '#11998e',
     lightGreen: '#38ef7d'
+  },
+  media: {
+    lteMedium: '(max-width: 768px)',
+    phone: '(max-width: 780px)',
+    tablet: '(max-width: 950px)',
+    lapTop: '(max-width: 1500px)',
+  },
+  media_screen_size: {
+    phone: '@media (max-width: 780px)',
+    tablet: '@media (max-width: 950px) and (min-width: 781px)',
+    lapTop: '@media (max-width: 1500px) and (min-width: 951px)',
+  },
+  padding: {
+    small: '1.6rem',
+    extra_small: '3.2rem',
+    large: '4.8rem',
+    extra_large: '6.4rem',
+    big: '8rem',
+    extra_big: '9.6rem',
   },
 } as const;
 
