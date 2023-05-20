feef8c937a5a99e1df7a7128395d0b49879b7156
João Eduardo <joaoeduardoj920@gmail.com>
2023-05-20 18:30:01 -0300

    feat(types): adding new types

--- src/types/index.ts
+++ src/types/index.ts
@@ -43,3 +43,64 @@ export type TLeagues = {
     }
   ]
 }
+
+export type TPlayers = {
+  response: [
+    {
+      player: {
+        age: number
+        id: number
+        name: string
+        nationality: string
+      }
+    }
+  ]
+}
+
+export type TSubFixtures = {
+  away: number
+  home: number
+  total: number
+}
+
+export type TSubGoals = {
+  percentage: string
+  total: number
+}
+
+export type TSubLineups = {
+  formation: string
+  played: number
+}
+
+export type TTeamStatistic = {
+  response: {
+      fixtures: {
+        draws: TSubFixtures
+        loses: TSubFixtures
+        played: TSubFixtures
+        wins: TSubFixtures
+      },
+      goals: {
+        against: {
+          minute: {
+            '0-15': TSubGoals
+            '16-30': TSubGoals
+            '31-45': TSubGoals
+            '46-60': TSubGoals
+            '61-75': TSubGoals
+            '76-90': TSubGoals
+            '91-105': TSubGoals
+            '106-120': TSubGoals
+        }
+        }
+      },
+      lineups: TSubLineups[],
+      team: {
+        id: number
+        logo: string
+        name: string
+      }
+    }
+
+}
